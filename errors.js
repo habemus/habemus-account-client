9335f6fac9b3409daca49298d77aa80dd0f196cf
Simon <simonxd@gmail.com>
2016-10-05 14:33:59 -0300

    gulpfile and development process

--- errors.js
+++ errors.js
@@ -1,8 +1,120 @@
+// native
 const util = require('util');
 
-var errors = require('../shared/errors');
+/**
+ * Base message constructor
+ * @param {String} message
+ */
+function HAccountError(message) {
+  Error.call(this);
 
-const HAccountError = errors.HAccountError;
+  this.message = message || 'HAccountError';
+};
+util.inherits(HAccountError, Error);
+HAccountError.prototype.name = 'HAccountError';
+
+/**
+ * Happens when any required option is invalid
+ *
+ * error.option should have the option that is invalid
+ * error.kind should contain details on the error type
+ * 
+ * @param {String} option
+ * @param {String} kind
+ * @param {String} message
+ */
+function InvalidOption(option, kind, message) {
+  HAccountError.call(this, message);
+
+  this.option = option;
+  this.kind = kind;
+}
+util.inherits(InvalidOption, HAccountError);
+InvalidOption.prototype.name = 'InvalidOption';
+
+/**
+ * Happens whenever the attempted username
+ * is already in use by another account.
+ * 
+ * @param {String} username
+ */
+function UsernameTaken(username) {
+  HAccountError.call(this, 'Username ' + username + ' is already taken');
+
+  this.username = username;
+}
+util.inherits(UsernameTaken, HAccountError);
+UsernameTaken.prototype.name = 'UsernameTaken';
+
+/**
+ * Happens whenever the attempted email
+ * is already in use by another account.
+ * 
+ * @param {String} email
+ */
+function EmailTaken(email) {
+  HAccountError.call(this, 'Username ' + email + ' is already taken');
+
+  this.email = email;
+}
+util.inherits(EmailTaken, HAccountError);
+EmailTaken.prototype.name = 'EmailTaken';
+
+/**
+ * Happens whenever the credentials provided are not valid
+ */
+function InvalidCredentials(detail) {
+  HAccountError.call(this, 'The credentials provided are invalid');
+
+  this.detail = detail;
+}
+util.inherits(InvalidCredentials, HAccountError);
+InvalidCredentials.prototype.name = 'InvalidCredentials';
+
+/**
+ * Happens whenever an action requested is not authorized
+ * by the server
+ * @param {String} message [description]
+ */
+function Unauthorized(message) {
+  HAccountError.call(this, message);
+}
+util.inherits(Unauthorized, HAccountError);
+Unauthorized.prototype.name = 'Unauthorized';
+
+/**
+ * Happens whenever the token provided for auth is invalid
+ */
+function InvalidToken() {
+  HAccountError.call(this, 'Token provided is invalid');
+}
+util.inherits(InvalidToken, HAccountError);
+InvalidToken.prototype.name = 'InvalidToken';
+
+/**
+ * Happens whenever the user is not found
+ * for the given identification (username or identifier)
+ * @param {String} identifier (username or identifier)
+ * @param {String} message
+ */
+function UserNotFound(identifier, message) {
+  HAccountError.call(this, message);
+
+  this.identifier = identifier;
+}
+util.inherits(UserNotFound, HAccountError);
+UserNotFound.prototype.name = 'UserNotFound';
+
+/**
+ * Happens when an account has to be active
+ * for an action to be performed, but the
+ * account has been cancelled
+ */
+function AccountCancelled() {
+  HAccountError.call(this);
+}
+util.inherits(AccountCancelled, HAccountError);
+AccountCancelled.prototype.name = 'AccountCancelled';
 
 /**
  * Client errors
@@ -17,7 +129,6 @@ function NotLoggedIn(message) {
 }
 util.inherits(NotLoggedIn, HAccountError);
 NotLoggedIn.prototype.name = 'NotLoggedIn';
-exports.NotLoggedIn = NotLoggedIn;
 
 /**
  * Happens whenever the user has cancelled any action.
@@ -28,6 +139,15 @@ function UserCancelled(message) {
 }
 util.inherits(UserCancelled, HAccountError);
 UserCancelled.prototype.name = 'UserCancelled';
-exports.UserCancelled = UserCancelled;
 
-Object.assign(exports, errors);
+exports.HAccountError = HAccountError;
+exports.InvalidOption = InvalidOption;
+exports.UsernameTaken = UsernameTaken;
+exports.EmailTaken = EmailTaken;
+exports.InvalidCredentials = InvalidCredentials;
+exports.Unauthorized = Unauthorized;
+exports.InvalidToken = InvalidToken;
+exports.UserNotFound = UserNotFound;
+exports.AccountCancelled = AccountCancelled;
+exports.NotLoggedIn = NotLoggedIn;
+exports.UserCancelled = UserCancelled;
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+// third-party dependencies
+const gulp       = require('gulp');
+const gulpSize   = require('gulp-size');
+const gulpUglify = require('gulp-uglify');
+
+const runSequence =  require('run-sequence');
+
+// browserify
+const browserify = require('browserify');
+const source     = require('vinyl-source-stream');
+const buffer     = require('vinyl-buffer');
+const gutil      = require('gulp-util');
+const brfs       = require('brfs');
+
+// browserSync
+const browserSync = require('browser-sync').create();
+
+/**
+ * Compile client library
+ */
+gulp.task('javascript:client', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './browser/index.js',
+    // debug: true,
+    // defining transforms here will avoid crashing your stream
+    transform: [],
+
+    // standalone global object for main module
+    standalone: 'HAccountClient'
+  });
+
+  return b.bundle()
+    .on('error', function (err) {
+      gutil.log('Browserify Error', err);
+      this.emit('end')
+    })
+    .pipe(source('h-account-client.js'))
+    .pipe(buffer())
+    .pipe(gulpUglify())
+    // calculate size before writing source maps
+    .pipe(gulpSize({
+      title: 'javascript:client',
+      showFiles: true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('javascript:client-ui', function () {
+  var b = browserify({
+    entries: './browser/ui/dialog/index.js',
+    transform: [brfs],
+
+    // standalone global object for main module
+    standalone: 'HAccountDialog'
+  });
+
+  return b.bundle()
+    .on('error', function (err) {
+      gutil.log('Browserify Error', err);
+      this.emit('end');
+    })
+    .pipe(source('h-account-dialog.js'))
+    .pipe(buffer())
+    .pipe(gulpSize({
+      title: 'javascript:client',
+      showFiles: true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('javascript', ['javascript:client', 'javascript:client-ui']);
+
+
+// Static server
+gulp.task('serve:client', function() {
+  browserSync.init({
+    server: {
+      baseDir: ['./demo', './dist']
+    },
+    open: true
+  });
+});
+
+/**
+ * Watch for changes and auto recompile
+ */
+gulp.task('watch', function () {
+
+  var clientFiles = [
+    'client/**/*.js',
+    'client/**/*.html',
+    'client/**/*.css',
+  ];
+
+  gulp.watch(clientFiles, ['javascript']);
+  gulp.watch([
+    'dist/**/*.js',
+    'demo/**/*'
+  ], browserSync.reload);
+});
+
+/**
+ * Main development task
+ */
+gulp.task('develop', function () {
+  runSequence('javascript', 'serve:client', 'watch');
+});
+
+gulp.task('default', ['develop']);
